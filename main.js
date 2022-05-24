c4e37689c94139e8a8768ac955cdf17273afc3b6
julian <julianmoreno242@gmail.com>
2022-05-24 17:45:53 -0500

    nueva version del proyecto

--- main.js
+++ main.js
@@ -1,19 +1,5 @@
 
-// function saludar(name,lastname,nickname){
-// return console.log(`Mi nombre es ${name} ${lastname} pero prefiero que me digas ${nickname}`);
-// };
 
-
-// saludar("julian","moreno","juliancho");
-let respuesta;
-const prompt = () => {
-     respuesta = parseInt(prompt("Dime cuanto es el resultado de 2 + 2 "));
-}
-
-prompt();
-
-if(respuesta === 4)console.log("Felicitaciones");
-else prompt();
 
 
 
